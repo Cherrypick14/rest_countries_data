bdc67b4e557d74d74efec9c52181add715608855
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-08 15:01:59 +0300

    add project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold text-red-600 '>I love coding </h1>
+
+    </div>
+  );
+}
+
+export default App;
