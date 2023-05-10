4569323f3c0a66939ef0c45c8ab2b82e03a05dd1
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-10 22:13:24 +0300

    finish article

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -5,10 +5,10 @@ const Article = ({flags,name, population,region,subregion}) => {
 
     <article>
 
-        <img src={flags.svg} alt=""/>
-        <h1>{name.common}</h1>
-        <ul>
-            <li>Population:{population}</li>
+        <img src={flags.svg} alt="" className='md:h-72 w-full object-cover '/>
+        <h2 className='font-bold mb-2 text-lg text-slate-950'>{name.common}</h2>
+        <ul className='flex flex-col items-start justify-start gap-2'>
+            <li>Population:{population.toLocaleString()}</li>
             <li>Region:{region}</li>
             <li>Subregion:{subregion}</li>
         </ul>
