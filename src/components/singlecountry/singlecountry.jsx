69b8980021552d77768e09e5ca7f6f387cd437fb
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-11 18:28:58 +0300

    add content to component

--- src/components/singlecountry/singlecountry.jsx
+++ src/components/singlecountry/singlecountry.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect}from 'react'
+import { useParams } from 'react-router-dom';
+
 
 const Singlecountry = () => {
+
+   const[country, setCountry] = useState([]);
+
+   const {name} = useParams();
+
+   useEffect(()=>{
+
+    const getCountryData = async () =>{
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+
+            const data = await res.json();
+
+            setCountry(data);
+
+        } catch (error) {
+          console.error(error)
+
+        }
+    }
+     
+   },[name])
   return (
-    <div>singlecountry</div>
+    <div>{name}</div>
   )
 }
 
