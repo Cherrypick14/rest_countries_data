15c07b62497b1b911478efd83a30cdb395b30c5a
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-11 17:26:22 +0300

    adding stylings to our articles

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -3,15 +3,20 @@ import React from 'react'
 const Article = ({flags,name, population,region,subregion}) => {
   return (
 
-    <article>
+    <article className='shadow-lg rounded-lg hover:drop-shadow-xl cursor-pointer '>
 
-        <img src={flags.svg} alt="" className='md:h-72 w-full object-cover '/>
-        <h2 className='font-bold mb-2 text-lg text-slate-950'>{name.common}</h2>
+        <img src={flags.svg} alt="" className='md:h-64 w-full object-cover '/>
+        
+         <div className="p-4">
+
+         <h2 className='font-bold mb-2 text-lg text-slate-950'>{name.common}</h2>
         <ul className='flex flex-col items-start justify-start gap-2'>
             <li>Population:{population.toLocaleString()}</li>
             <li>Region:{region}</li>
             <li>Subregion:{subregion}</li>
         </ul>
+         </div>
+       
     </article>
   )
 }
