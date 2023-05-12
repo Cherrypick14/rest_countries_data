564508bc2cd9052b60288068d6054e7fe022d7df
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-12 16:59:54 +0300

    finish borders

--- src/components/singlecountry/singlecountry.jsx
+++ src/components/singlecountry/singlecountry.jsx
@@ -11,6 +11,7 @@ const Singlecountry = () => {
    useEffect(()=>{
 
     const getCountryData = async () =>{
+
         try {
             const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
 
@@ -23,10 +24,51 @@ const Singlecountry = () => {
 
         }
     }
+      getCountryData();
      
    },[name])
   return (
-    <div>{name}</div>
+
+    <section className='max-w-7xl p-8 md:py-0 mx-auto'>
+
+      {country.map((item)=>(
+     <div className='grid grid-cols-1 md:grid-cols-2 h-screen gap-8 md:place-items-center'>
+
+      <article>
+        <img src={item.flags.svg} alt={item.name.common} />
+      </article>
+
+      <article>
+        <h1 className='font-bold text-4xl text-gray-700 mb-4'>{item.name.official}</h1>
+
+         <ul>
+          <li>Capital:{item.capital[0]}</li>
+          <li>Population:{item.population.toLocaleString()}</li>
+          <li>Region:{item.region}</li>
+          <li>SubRegion:{item.subregion}</li>
+  
+         </ul>
+         
+         {item.borders && (
+          <div className='mt-5'>
+           <h3 className='font-bold text-2xl text-gray-800 mb-3'>Borders</h3>
+              <ul className='flex flex-col md:flex-row gap-3'>
+             
+                {item.borders.map((borders, index)=>(
+                  <li key={index} className='bg-slate-50 rounded-md px-3 py-3 w-fit'>{borders}</li>
+                ))}
+   
+            </ul>
+          </div>
+          
+         )}
+        
+      </article>
+     </div>
+
+      ))}
+      
+    </section>
   )
 }
 
