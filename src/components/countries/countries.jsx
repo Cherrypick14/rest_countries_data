5be27d767b430c1fb68d3dd97e68983fe93910e1
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-12 22:04:17 +0300

    add our forms

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -27,7 +27,23 @@ const Countries = () => {
     
   return (
     <section className='container mx-auto p-8'>
+
         {/* {forms} */}
+        <div className='mb-8 flex flex-col gap-4 md:flex-row md:justify-between'>
+            <form autoComplete='off' className='max-w-3xl md:flex-1'>
+
+            <input type="text" name="search" id="search" placeholder='Start your Country Search...' className='outline-none px-2 py-4 placeholder-slate-600 w-full shadow rounded' required />
+
+            </form>
+
+            <form className='md:flex-2'>
+
+              <select type='text' name="filter-by-region" id="filter-by-region" className='w-52'></select>
+
+            </form>
+           
+        </div>
+
         <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 
         {countries.map((country)=>(
