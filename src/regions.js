cf1fa00a948faab623683b41668fd4e65718a2a6
Cherrypick14 <cherrypicklet@gmail.com>
2023-05-15 18:48:32 +0300

    remove typos

--- src/regions.js
+++ src/regions.js
@@ -1,31 +1,31 @@
 export const regions =[
     {
-    id:1,
+   
     name:"Europe"
     },
 
     {
-    id:2,
+    
     name:"Asia"
     },
 
      {
-    id:3,
+    
     name:"Africa"
      },
 
     {
-    id:4,
+    
     name:"Oceania"
      },
 
     {
-    id:5,
+    
     name:"America"
      },
      
     {
-    id:6,
+   
     name:"Antarctic"
     },
 ]
